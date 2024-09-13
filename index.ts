7cd821c125a012f37f8cbbe0df7e1a2ef800a023
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-09-13 22:49:36 +0700

    tao route

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("OK");
+import express, { Express, Request, Response} from "express";
+const app: Express = express();
+const port: number | string = process.env.PORT || 3000;
+
+app.get("/topics", (req: Request, res: Response) => {
+  res.send("Chu de bai hat");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+})
