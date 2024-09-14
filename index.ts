4721017a2a7c3047ee99673ce87a67d38d16ffea
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-09-14 15:31:43 +0700

    tao route

--- index.ts
+++ index.ts
@@ -1,10 +1,9 @@
-import express, { Express, Request, Response} from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
 import { connectDatabase } from "./config/connectDB";
 
-import Topic from "./models/topic.model";
 
 connectDatabase();
 
@@ -14,16 +13,9 @@ const port: number | string = process.env.PORT || 3000;
 app.set('views', './views')
 app.set('view engine', 'pug');
 
+import {routesClient} from "./routes/client/index.route";
+routesClient(app);
 
-app.get("/topics", async (req: Request, res: Response) => {
-  const topics = await Topic.find({
-    deleted: false
-  });
-
-  console.log(topics);
-
-  res.render("client/pages/topics/index");
-});
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
