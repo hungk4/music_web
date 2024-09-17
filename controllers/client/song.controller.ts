d0db0c290072805f19a5a5e0f61d8318f818d279
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-09-17 20:54:03 +0700

    lam trang chi tiet bai hat

--- controllers/client/song.controller.ts
+++ controllers/client/song.controller.ts
@@ -5,30 +5,66 @@ import Singer from "../../models/singer.model";
 
 // [GET] /songs/:slugTopic
 export const list = async (req: Request, res: Response) => {
-  const slugTopic: string = req.params.slugTopic;
+  try{
+    const slugTopic: string = req.params.slugTopic;
 
-  const topic = await Topic.findOne({
-    slug: slugTopic,
-    deleted: false,
-    status: "active"
-  });
-
-  const songs = await Song.find({
-    topicId: topic.id,
-    deleted: false,
-    status: "active"
-  }).select("title avatar singerId like slug");
-
-  for (const item of songs) {
-    const singerInfo = await Singer.findOne({
-      _id: item.singerId
-    }).select("fullName");
-
-    item["singerFullName"] = singerInfo["fullName"];
+    const topic = await Topic.findOne({
+      slug: slugTopic,
+      deleted: false,
+      status: "active"
+    });
+  
+    const songs = await Song.find({
+      topicId: topic.id,
+      deleted: false,
+      status: "active"
+    }).select("title avatar singerId like slug");
+  
+    for (const item of songs) {
+      const singerInfo = await Singer.findOne({
+        _id: item.singerId
+      }).select("fullName");
+  
+      item["singerFullName"] = singerInfo["fullName"];
+    }
+  
+    res.render("client/pages/songs/list", {
+      pageTitle: topic.title,
+      songs: songs
+    });
+  } catch(e){
+    res.redirect("back");
   }
-
-  res.render("client/pages/songs/list", {
-    pageTitle: topic.title,
-    songs: songs
-  });
 };
+
+// [GET] /songs/detail/:slugSong
+export const detail = async (req: Request, res: Response) => {
+  try{
+    const slugSong: string = req.params.slugSong;
+
+    const song = await Song.findOne({
+      slug: slugSong,
+      status: "active",
+      deleted: false
+    });
+
+    const singer = await Singer.findOne({
+      _id: song.singerId,
+      deleted: false
+    }).select("fullName")
+
+    const topic = await Topic.findOne({
+      _id: song.topicId,
+      deleted: false
+    }).select("title")
+
+    res.render("client/pages/songs/detail.pug", {
+      pageTitle: "Chi tiết bài hát",
+      song: song,
+      topic: topic,
+      singer: singer
+    })
+  } catch(e){
+    res.redirect("back");
+  }
+}
