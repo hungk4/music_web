92b60bbd715eac029a954763e019957d3519d1f1
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-09-17 20:23:31 +0700

    lam trang danh sach bai hat

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -1,7 +1,9 @@
 import { Express } from "express";
-
-import  {topicRoute}  from "./topic.route"
+import  { topicRoute }  from "./topic.route";
+import  { songRoute }  from "./song.route";
 
 export const routesClient = (app: Express) => {
-  app.use("/topics", topicRoute)
+  app.use("/topics", topicRoute);
+  
+  app.use("/songs", songRoute);
 }
