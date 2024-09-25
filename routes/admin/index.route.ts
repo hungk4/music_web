2c247c8a7453b2d97519f5c4a4b5bc2d0a58c4fb
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-09-25 20:51:30 +0700

    tao trang danh sach bai hat (ben admin)

--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -1,6 +1,7 @@
 import { Express } from "express";
 import { dashboardRoute }from "./dashboard.route";
 import { topicRoute } from "./topic.route";
+import { songRoute } from "./song.route";
 import {systemConfig} from "../../config/system";
 
 export const routesAdmin = (app: Express) => {
@@ -9,4 +10,6 @@ export const routesAdmin = (app: Express) => {
   app.use(`${PATH}/dashboard`, dashboardRoute);
 
   app.use(`${PATH}/topics`, topicRoute);
+
+  app.use(`${PATH}/songs`, songRoute);
 }
