87baf0a965c0f9316e51ac0d02633dfb627437ae
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-09-26 15:55:49 +0700

    create bai hat

--- controllers/admin/song.controller.ts
+++ controllers/admin/song.controller.ts
@@ -3,6 +3,8 @@ import Song from "../../models/song.model";
 import Topic from "../../models/topic.model";
 import Singer from "../../models/singer.model";
 
+import { systemConfig } from "../../config/system";
+
 // [GET] /admin/songs
 export const index = async (req: Request, res: Response) => {
   const songs = await Song.find({
@@ -28,3 +30,10 @@ export const create = async (req: Request, res: Response) => {
     singers: singers
   });
 };
+
+// [POST] /admin/songs/create
+export const createPost = async (req: Request, res: Response) => {
+  const song = new Song(req.body);
+  await song.save();
+  res.redirect(`/${systemConfig.prefixAdmin}/songs`);
+}
--- routes/admin/song.route.ts
+++ routes/admin/song.route.ts
@@ -1,10 +1,21 @@
 import express from "express";
+import multer from "multer";
 const router = express.Router();
 
+const upload = multer();
+
 import * as controller from "../../controllers/admin/song.controller";
+import * as uploadCoud from "../../middlewares/admin/uploadCloud.middelware";
 
 router.get("/", controller.index);
 
 router.get("/create", controller.create);
+
+router.post(
+  "/create",
+  upload.single("avatar"),
+  uploadCoud.uploadSingle,
+  controller.createPost
+);
 
 export const songRoute = router;
